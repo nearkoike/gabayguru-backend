187fb6929e773ff5db051a5abdfb6ab340645fc9
nyel_gulmatico <daniel.gulmatico@xurpas.com>
2024-06-28 14:12:01 +0800

    fix show

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,47 +16,47 @@ class UserController extends Controller
     public function login(LoginRequest $request)
     {
         $credentials = $request->only('email', 'username', 'password');
-    
+
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
             $token = $user->createToken('gabay-guru')->plainTextToken;
-    
+
             $user['token'] = $token;
-    
+
             return response()->json($user, 200);
         }
-    
+
         return response()->json(['message' => 'Invalid email or password.'], Response::HTTP_UNAUTHORIZED);
     }
 
     public function register(StoreUserRequest $request)
     {
-        $imageName = time().'.'.$request->file('profile_picture')->extension();  
+        $imageName = time() . '.' . $request->file('profile_picture')->extension();
         $request->file('profile_picture')->move(public_path('images'), $imageName);
 
         $user = User::create(array_merge($request->validated(), [
             'password' => bcrypt($request->input('password')),
             'role' => 1,
-            'profile_picture' => url('/') .'/images/'. $imageName
+            'profile_picture' => url('/') . '/images/' . $imageName
         ]));
 
         $email = $request->input('email');
         $name = $request->input('first_name') . " " . $request->input('last_name');
 
         // The email sending is done using the to method on the Mail facade
-        
+
         // Mail::to($email)->send(new MyTestEmail($name));
-        
+
         $token = $user->createToken('gabay-guru');
         $user['token'] = $token->plainTextToken;
-                
+
         return response()->json($user, 200);
     }
 
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
-    
+
         return response()->json([
             'message' => 'Successfully logged out'
         ], 200);
@@ -67,15 +67,15 @@ class UserController extends Controller
     public function index()
     {
         $usersResource = UserResource::collection(User::with([
-            'support_tickets', 
-            'student_tickets', 
-            'schedules', 
-            'transactions', 
-            'student_appointments', 
+            'support_tickets',
+            'student_tickets',
+            'schedules',
+            'transactions',
+            'student_appointments',
             'mentor_appointments',
             'penalties'
         ])->get());
-        return json_encode( $usersResource, 200);
+        return json_encode($usersResource, 200);
     }
 
     /**
@@ -91,31 +91,31 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        $imageName = time().'.'.$request->file('profile_picture')->extension();  
+        $imageName = time() . '.' . $request->file('profile_picture')->extension();
         $request->file('profile_picture')->move(public_path('images'), $imageName);
 
         $user = User::create(array_merge($request->validated(), [
             'password' => bcrypt($request->input('password')),
-            'profile_picture' => url('/') .'/images/'. $imageName
+            'profile_picture' => url('/') . '/images/' . $imageName
         ]));
 
         $email = $request->input('email');
         $name = $request->input('first_name') . " " . $request->input('last_name');
-        
+
         $token = $user->createToken('gabay-guru');
         $user['token'] = $token->plainTextToken;
 
         $userRelationship = User::with([
-            'support_tickets', 
-            'student_tickets', 
-            'schedules', 
-            'transactions', 
-            'student_appointments', 
+            'support_tickets',
+            'student_tickets',
+            'schedules',
+            'transactions',
+            'student_appointments',
             'mentor_appointments'
         ])->find($user->id);
-        
+
         $userResource = new UserResource($userRelationship);
-        return json_encode( $userResource, 200);
+        return json_encode($userResource, 200);
     }
 
     /**
@@ -123,7 +123,16 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        $userResource = new UserResource($user);
+        $userRelationship = User::with([
+            'support_tickets',
+            'student_tickets',
+            'schedules',
+            'transactions',
+            'student_appointments',
+            'mentor_appointments'
+        ])->find($user->id);
+
+        $userResource = new UserResource($userRelationship);
 
         return response()->json($userResource, 200);
     }
@@ -143,35 +152,34 @@ class UserController extends Controller
     {
         $photoPath = $user->profile_picture;
         $password = $user->password;
-    
+
         if ($request->hasFile('profile_picture')) {
-            $imageName = time().'.'.$request->file('profile_picture')->getClientOriginalExtension();
+            $imageName = time() . '.' . $request->file('profile_picture')->getClientOriginalExtension();
             $request->file('profile_picture')->move(public_path('images'), $imageName);
-            $photoPath = url('/images/'.$imageName);
+            $photoPath = url('/images/' . $imageName);
         }
 
         if ($request->filled('password')) {
             $password = bcrypt($request->input('password'));
         }
-    
+
         $user->fill(array_merge($request->validated(), [
             'password' => $password,
             'profile_picture' => $photoPath
         ]));
         $user->save();
-    
+
         $userRelationship = User::with([
-            'support_tickets', 
-            'student_tickets', 
-            'schedules', 
-            'transactions', 
-            'student_appointments', 
+            'support_tickets',
+            'student_tickets',
+            'schedules',
+            'transactions',
+            'student_appointments',
             'mentor_appointments'
         ])->find($user->id);
 
         $userResource = new UserResource($userRelationship);
-        return json_encode( $userResource, 200);
-
+        return json_encode($userResource, 200);
     }
 
     /**
@@ -180,13 +188,13 @@ class UserController extends Controller
     public function destroy($id)
     {
         $user = User::find($id);
-        
+
         if (!$user) {
             return response()->json("User not found", 404);
         }
-        
+
         $user->delete();
-    
+
         return response()->json("Deleted user id: " . $id, 200);
     }
 }
