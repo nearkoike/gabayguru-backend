51794f57101614b966a3080a048edba7fff108a3
nyel_gulmatico <daniel.gulmatico@xurpas.com>
2024-07-03 19:27:04 +0800

    change dfault

--- database/migrations/2024_07_03_095525_add_status_to_users_table.php
+++ database/migrations/2024_07_03_095525_add_status_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('status')->default("PENDING")->after('role');
+            $table->string('status')->default("FOR_VERIFICATION")->after('role');
         });
     }
 
